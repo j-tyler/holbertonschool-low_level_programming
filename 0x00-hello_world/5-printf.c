fd359a42e971092dea11c749a527ec3636ce956e
j-tyler <justin.marsh@holbertonschool.com>
2016-10-12 21:24:10 +0000

    5. Comment formatting fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - a simple example of using printf
+ *
+ * Return: Always 0
+ */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+	return (0);
 }
