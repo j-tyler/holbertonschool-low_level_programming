18fb5cf11d58deb44cb9c5caac19edce0748f9af
j-tyler <justin.marsh@holbertonschool.com>
2016-10-12 21:43:09 +0000

    6. Commenting and Formatting fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,10 @@
 
 int main(void)
 {
-	printf("Size of a char: %d bytes(s)\n", (int)(sizeof(char)));
-	printf("Size of a int: %d bytes(s)\n", (int)(sizeof(int)));
-	printf("Size of a long int: %d bytes(s)\n", (int)(sizeof(long)));
-	printf("Size of a long long int: %d bytes(s)\n", (int)(sizeof(long long)));
-	printf("Size of a float: %d bytes(s)\n", (int)(sizeof(float)));
+	printf("Size of a char: %d byte(s)\n", (int)(sizeof(char)));
+	printf("Size of a int: %d byte(s)\n", (int)(sizeof(int)));
+	printf("Size of a long int: %d byte(s)\n", (int)(sizeof(long)));
+	printf("Size of a long long int: %d byte(s)\n", (int)(sizeof(long long)));
+	printf("Size of a float: %d byte(s)\n", (int)(sizeof(float)));
 	return (0);
 }
