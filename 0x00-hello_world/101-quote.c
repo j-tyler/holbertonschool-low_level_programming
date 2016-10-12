c4b752fbb9a0bfde97e5755d8be839871f67ffb8
j-tyler <justin.marsh@holbertonschool.com>
2016-10-12 21:48:50 +0000

    9. Formatting fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,14 @@
 #include <unistd.h>
 #include <stdio.h>
 
+/**
+ * main - Changes stderr without using printf or puts
+ *
+ * Return: Always 1
+ */
+
 int main(void)
 {
-	write(2,"and that piece of art is useful - Dora Korpar, 2015-10-19\n",58);
-	return 1;
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
 }
