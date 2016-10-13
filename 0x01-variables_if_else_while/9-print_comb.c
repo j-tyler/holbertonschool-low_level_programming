0546123cb9e605a14610bc29a0c63391d34e324f
j-tyler <justin.marsh@holbertonschool.com>
2016-10-13 22:46:03 +0000

    9. Comma seperated numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print all base10 numbers comma seperated without printf
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (i < 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
