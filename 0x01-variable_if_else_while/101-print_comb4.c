731be2ade7f8ab98e306720bd29b8a00d36e7338
j-tyler <justin.marsh@holbertonschool.com>
2016-10-13 16:58:35 +0000

    12. The success combination in business is: Do what you do better... and: do more of what you do..

---
+++ 0x01-variable_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - print all numbers 012 to 789 where
+ * first numb < second numb < thrid numb
+ *
+ * Return: Void
+ */
+
+void main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			for (k = 0; k < 10; k++)
+			{
+				if (i < j && j < k)
+				{
+					putchar(i + 48);
+					putchar(j + 48);
+					putchar(k + 48);
+					if (i < 7)
+					{
+						putchar(44);
+						putchar(32);
+					}
+				}
+			}
+		}
+	}
+	putchar(10);
+}
