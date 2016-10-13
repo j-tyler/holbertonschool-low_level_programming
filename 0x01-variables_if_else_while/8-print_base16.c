5ce0b26cd97be405fe3d2d3ae056e0607722b6a9
j-tyler <justin.marsh@holbertonschool.com>
2016-10-13 21:51:59 +0000

    8. Hexadecimal -formatting

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,8 +10,10 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 16; i++)
-		printf("%x", i);
+	for (i = 48; i < 58; i++)
+		putchar(i);
+	for (i = 97; i < 103; i++)
+		putchar(i);
 	putchar('\n');
 	return (0);
 }
