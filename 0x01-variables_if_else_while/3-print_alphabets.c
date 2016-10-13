c2a22798df0899510474a26675f35725a959cb6e
j-tyler <justin.marsh@holbertonschool.com>
2016-10-13 21:20:58 +0000

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print out the alphabet
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 97; c < 123; c++)
+		putchar(c);
+	for (c = 65; c < 91; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
