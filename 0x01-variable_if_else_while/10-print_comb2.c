6a37f82bb489ad41107a0c8eebac783b28fe6bfb
j-tyler <justin.marsh@holbertonschool.com>
2016-10-13 16:39:32 +0000

    00..99

---
+++ 0x01-variable_if_else_while/10-print_comb2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - printout all numbers from 0 to 99
+ *
+ * Return: Void
+ */
+
+void main(void)
+{
+	int n, i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			putchar(i + 48);
+			putchar(j + 48);
+			if (i * j != 81)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+	putchar(10);
+}
