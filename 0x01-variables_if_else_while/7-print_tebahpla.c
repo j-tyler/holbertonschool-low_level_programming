3fc18b2ed5128d9df2409cb53a06f49e02d14a61
j-tyler <justin.marsh@holbertonschool.com>
2016-10-13 21:44:52 +0000

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet in reverse
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 122; i > 96; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
