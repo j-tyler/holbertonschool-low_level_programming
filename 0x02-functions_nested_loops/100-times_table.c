85eaef00c9446d5af0e7342cf3970d70c954c7d7
j-tyler <justin.marsh@holbertonschool.com>
2016-10-18 17:34:48 +0000

    12. Replace printf with _putchar

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "holberton.h"
 
 /**
@@ -10,20 +9,40 @@
 
 void print_times_table(int n)
 {
-	int i, j;
+	int i, j, k;
 
 	if (n < 0 || n > 15)
-		printf("\n");
+		;
 	else
 	{
 		for (i = 0; i <= n; i++)
 		{
-			printf("0");
+			_putchar('0');
 			for (j = 1; j <= n; j++)
 			{
-				printf(", %3d", j * i);
+				k = i * j;
+				_putchar(',');
+				_putchar(' ');
+				if (k < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				}
+				else if (k < 100)
+				{
+					_putchar(' ');
+					_putchar(k / 10 + '0');
+					_putchar(k % 10 + '0');
+				}
+				else
+				{
+					_putchar(k / 100 + '0');
+					_putchar(k % 100 / 10 + '0');
+					_putchar(k % 10 + '0');
+				}
 			}
-			printf("\n");
+			_putchar('\n');
 		}
 	}
 }
