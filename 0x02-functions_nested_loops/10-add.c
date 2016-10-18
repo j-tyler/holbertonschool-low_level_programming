322726e097b1ea82a9254aab6aff81f3060ff105
j-tyler <justin.marsh@holbertonschool.com>
2016-10-18 16:55:40 +0000

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * add - add two args and return result
+ * @a: int to add
+ * @b: int to add
+ *
+ * Return: result of addition of arguments
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
