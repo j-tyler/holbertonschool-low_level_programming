e6bfe5fa4dce1ca57c2450a92130106cdf133a69
j-tyler <justin.marsh@holbertonschool.com>
2016-10-18 17:07:30 +0000

    11. Holberotn School, 98 Battery Street

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * print_to_98 - prints all numbers from n to 98
+ * @n: number to start count from
+ *
+ * Return: 0
+ */
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+		for( ; n < 98; n++)
+			printf("%d, ", n);
+	else
+		for ( ; n > 98; n--)
+			printf("%d, ", n);
+	printf("%d\n", n);
+}
