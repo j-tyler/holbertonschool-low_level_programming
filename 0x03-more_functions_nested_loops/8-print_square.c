35636958426a49a2da392ea9901459f54e3dec92
j-tyler <justin.marsh@holbertonschool.com>
2016-10-20 18:16:09 +0000

    8. Print # squares

---
+++ 0x03-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_square - create a pound square of size arg
+ * @size: size of the square
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= size; j++)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
