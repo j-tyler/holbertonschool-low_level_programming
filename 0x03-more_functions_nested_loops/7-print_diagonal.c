2d44c554ed8cd8b3c57e82046983b1f2fa7f72ce
j-tyler <justin.marsh@holbertonschool.com>
2016-10-20 18:09:10 +0000

    7. print diagonals

---
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - print a diagonal of length arg
+ * @n: length of diagonal to print
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (j = 1; j <= n; j++)
+	{
+		for (i = 1; i < j; i++)
+			_putchar(' ');
+	
+		if (n > 0)
+		{
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	if (n <= 0)
+		_putchar('\n');
+}
