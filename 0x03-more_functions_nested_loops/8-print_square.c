a2c616caa2cee7e06f4f140ee36997bf20f88098
j-tyler <justin.marsh@holbertonschool.com>
2016-10-21 05:15:40 +0000

    added newline for 0 or less size

--- 0x03-more_functions_nested_loops/8-print_square.c
+++ 0x03-more_functions_nested_loops/8-print_square.c
@@ -9,10 +9,14 @@ void print_square(int size)
 {
 	int i, j;
 
-	for (i = 1; i <= size; i++)
-	{
-		for (j = 1; j <= size; j++)
-			_putchar('#');
+	if (size < 1)
 		_putchar('\n');
-	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
 }
