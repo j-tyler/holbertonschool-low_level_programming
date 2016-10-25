6ee4fb6b66cea14c48e5e286dc70553719c3ee75
j-tyler <justin.marsh@holbertonschool.com>
2016-10-25 20:52:26 +0000

    Add Solution 3 0x04

---
+++ 0x04-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _puts - prints out a string
+ * @str: string to print
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
--- 0x04-pointers_arrays_strings/holberton.h
+++ 0x04-pointers_arrays_strings/holberton.h
@@ -6,7 +6,7 @@ char *_strcpy(char *dest, char *src);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
-
+void _puts(char *str);
 
 
 
