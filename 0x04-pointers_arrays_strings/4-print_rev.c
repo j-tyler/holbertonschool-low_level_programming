432d9cec36fe2f2cdc5bce2685df8d62d4d2622c
j-tyler <justin.marsh@holbertonschool.com>
2016-10-25 20:56:41 +0000

    Add Solution 4 0x04

---
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_rev - print a string in reverse
+ * @s: String to be printed
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	for (i = i - 1 ; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
