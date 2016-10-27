2d1271404711984999d6f032e7af272ba32105dd
j-tyler <justin.marsh@holbertonschool.com>
2016-10-27 23:38:22 +0000

    Add solution #4 0x05

---
+++ 0x05-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - Reverse the content of an array
+ * @a: array to reverse
+ * @n: number of elements
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, tmp;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		tmp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = tmp;
+	}
+}
