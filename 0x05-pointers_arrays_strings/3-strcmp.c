df912f1a185777ed642c43aa58ea55856e8f7333
j-tyler <justin.marsh@holbertonschool.com>
2016-10-27 21:58:35 +0000

    Add solution 3 0x05wq

---
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _strcmp - Compare strings, return value difference
+ * @s1: first string to compare
+ * @s2: second string to compare
+ *
+ * Return: Value difference of strings
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (*(s1 + i) == *(s2 + i) && *(s1 + i) != '\0' && *(s2 + i) != '\0')
+		i++;
+
+	return (*s1 - *s2);
+}
