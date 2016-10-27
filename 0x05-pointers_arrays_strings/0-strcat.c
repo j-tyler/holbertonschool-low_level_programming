f8a1eeb99fd4780957716f8c6762cae6d8342b33
j-tyler <justin.marsh@holbertonschool.com>
2016-10-27 21:07:55 +0000

    Add solution 0 0x05

---
+++ 0x05-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strcat - concatenate string arg2 to arg1
+ * @dest: destination
+ * @src: source to concatenate to destination
+ *
+ * Return: pointer to destination
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	j = 0;
+	for (i = 0; dest[i] != '\0'; i++)	
+		;
+	do
+	{
+		*(dest + i + j) = *(src + j);
+		j++;
+	} while (*(src + j));
+
+	return (dest);
+}
