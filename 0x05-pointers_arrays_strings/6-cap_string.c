7e1b5e68bc70a9acfab21bb19ebb85fefe54ebad
j-tyler <justin.marsh@holbertonschool.com>
2016-10-28 00:03:53 +0000

    Add solution #6 0x05

---
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+
+/**
+ * cap_string - capitalize all words of string
+ * @s: string to capitalize
+ *
+ * Return: pointer to string
+ */
+
+char *cap_string(char *s)
+{
+	char *temp;
+	char prev;
+
+	temp = s;
+	do {
+		if (*s <= 'z' && *s >= 'a')
+		{
+			prev = *(s - 1);
+			switch (prev)
+			{
+			case ',':
+			case ';':
+			case '.':
+			case '!':
+			case '?':
+			case '"':
+			case '(':
+			case ')':
+			case '{':
+			case '}':
+			case ' ':
+			case '\t':
+			case '\n':
+				*s -= 32;
+				break;
+			}
+		}
+	} while (*s++);
+	return (temp);
+}
