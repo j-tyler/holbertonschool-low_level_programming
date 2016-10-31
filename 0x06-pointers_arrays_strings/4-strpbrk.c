bfb6729bc9cca18ac50e92fb24f9b3b41fa048a3
j-tyler <justin.marsh@holbertonschool.com>
2016-10-31 18:40:54 +0000

    Add solution 4 0x06

---
+++ 0x06-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strpbrk - Search string for first occurence that matches accept
+ * @s: String to search
+ * @accept: Chars to search for match
+ *
+ * Return: Pointer to first occurence in string of chars in accept
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int flag, c;
+
+	flag = 1;
+	do {
+		c = 0;
+		while (*(accept + c) != '\0' && flag)
+		{
+			if (*(accept + c) == *s)
+				flag = 0;
+			c++;
+		}
+	} while (flag && *s++);
+	return (s);
+}
