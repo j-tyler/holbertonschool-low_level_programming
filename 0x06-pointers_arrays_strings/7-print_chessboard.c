43467653fd810b1d32558a808164ca1e0da7daf0
j-tyler <justin.marsh@holbertonschool.com>
2016-10-31 19:08:27 +0000

    Add solution 7 0x06

---
+++ 0x06-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_chessboard - print out a chessboard
+ * @(*a)[8]: Layout to print
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++, *a++)
+	{
+		for (j = 0; j < 8; j++)
+			_putchar((*a)[j]);
+		_putchar('\n');
+	}	
+}
