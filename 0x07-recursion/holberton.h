196d1eb7ac6173b24f5245b3f6355a1e492e7668
j-tyler <justin.marsh@holbertonschool.com>
2016-11-02 19:06:11 +0000

    Add Solution 1 0x07 Print string in reverse

--- 0x07-recursion/holberton.h
+++ 0x07-recursion/holberton.h
@@ -1,6 +1,8 @@
+int _putchar(char c);
 int is_palindrome(char *s);
 int is_prime_number(int n);
 int _sqrt_recursion(int n);
 int _pow_recursion(int x, int y);
 int wildcmp(char *s1, char *s2);
 void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
