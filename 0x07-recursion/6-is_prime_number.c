441720b47763fafc23223b5ae3e85bf378ca6142
j-tyler <justin.marsh@holbertonschool.com>
2016-11-02 17:09:48 +0000

    Add Solution 6 0x07 is prime number

---
+++ 0x07-recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * is_prime_number - Determine if a number is prime
+ * @n: number to evaluate
+ *
+ * Return: 1 if n is prime, else 0
+ */
+int is_prime_number(int n)
+{
+	static int tester = 2;
+
+	if (n < 2)
+	{
+		return (0);
+	}
+	else if (n < tester * 2)
+	{
+		tester = 2;
+		return (1);
+	}
+	else if (n % tester == 0)
+	{
+		tester = 2;
+		return (0);
+	}
+	else
+	{
+		tester++;
+		return (is_prime_number(n));
+	}
+}
---
+++ 0x07-recursion/holberton.h
@@ -0,0 +1,2 @@
+int is_palindrome(char *s);
+int is_prime_number(int n);
