33f168a170be67fb709c8d21d3144ad769b3ad9c
j-tyler <justin.marsh@holbertonschool.com>
2016-11-02 18:09:07 +0000

    Solution 8 0x07 Wild Comparasion

---
+++ 0x07-recursion/100-wildcmp.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * wildcmp - Compare two strings, find if they are matching
+ * @s1: String to compare
+ * @s2: String to compare, can include * wildcard
+ *
+ * Return: 1 if identical, else 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	else if (*s1 == '\0' && *s2 == '*')
+		return (wildcmp(s1, (s2 + 1)));
+	else if (*s1 == '\0' || *s2 == '\0')
+		return (0);
+
+	if (*s1 == *s2)
+		return (wildcmp((s1 + 1), (s2 + 1)));
+	/* currently does not work for repeating s1 strings */
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, (s2 + 1)));
+		else if (*s1 == *(s2 + 1))
+			return (wildcmp(s1, (s2 + 1)));
+		else if (*s1 != *(s2 + 1))
+			return (wildcmp((s1 + 1), s2));
+	}
+	return (0);
+}
--- 0x07-recursion/holberton.h
+++ 0x07-recursion/holberton.h
@@ -2,3 +2,4 @@ int is_palindrome(char *s);
 int is_prime_number(int n);
 int _sqrt_recursion(int n);
 int _pow_recursion(int x, int y);
+int wildcmp(char *s1, char *s2);
