9852cdf120916d521dd8ec5dfb14232da79c91ea
j-tyler <justin.marsh@holbertonschool.com>
2016-11-03 03:17:34 +0000

    Redo 5 0x07, move old solution to new file

--- 0x07-recursion/5-sqrt_recursion.c
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -1,6 +1,28 @@
 #include "holberton.h"
 
 /**
+ * _sqrt_moreparams - Non-static resursive helper for finding sqrt
+ * @n: number to find square
+ * @c: numbers we have checked
+ *
+ * Return: The Square Root if rational, or -1
+ */
+int _sqrt_moreparams(int n, int c)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (0);
+
+	if (c * c > n)
+		return (-1);
+	else if (c * c == n)
+		return (c);
+	else
+		return (_sqrt_moreparams(n, c + 1));
+}
+
+/**
  * _sqrt_recursion - find if a number is a natual square root
  * @n: number to evaluate
  *
@@ -8,28 +30,5 @@
  */
 int _sqrt_recursion(int n)
 {
-	static int c = 1;
-	int tmp;
-
-	if (n < 0)
-		return (-1);
-	else if (n == 0)
-		return (0);
-
-	if (c * c > n)
-	{
-		c = 1;
-		return (-1);
-	}
-	else if (c * c == n)
-	{
-		tmp = c;
-		c = 1;
-		return (tmp);
-	}
-	else
-	{
-		c++;
-		return (_sqrt_recursion(n));
-	}
+	return (_sqrt_moreparams(n, 1));
 }
