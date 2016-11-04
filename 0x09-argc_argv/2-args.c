8884efd32492e8914fe22f30a80524e977fb9afc
j-tyler <justin.marsh@holbertonschool.com>
2016-11-04 18:02:53 +0000

    Add solution 2 0x09

---
+++ 0x09-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Print all arguments recieved
+ * argc: number of arguments
+ * argv: argument array
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	i = 1;
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
