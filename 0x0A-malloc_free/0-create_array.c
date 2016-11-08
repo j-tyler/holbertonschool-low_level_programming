a78dbf921442c1298dfb54c2a7347e638a143eb7
j-tyler <justin.marsh@holbertonschool.com>
2016-11-08 20:28:37 +0000

    Add solution 0 0x0A

---
+++ 0x0A-malloc_free/0-create_array.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * create_array - Create a char array, filled with a specific character
+ * @size: Size of the array
+ * @c: char to fill array with
+ *
+ * Return: pointer to array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+	unsigned int i;
+
+	p = malloc(size * sizeof(char));
+	for (i = 0; i < size; i++)
+		*(p + i) = c;
+	return (p);
+}
