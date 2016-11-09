733360395228b8dd4c14e647e2feac0a6031b3bb
j-tyler <justin.marsh@holbertonschool.com>
2016-11-09 01:35:07 +0000

    add FREE 3 0x0A

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -16,11 +16,17 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 
 	pgrid = malloc(height * sizeof(void *));
+	if (pgrid == NULL)
+		return (NULL);
 	for (i = 0; i < height; i++)
 	{
 		*(pgrid + i) = malloc(width * sizeof(int));
 		if (*(pgrid + i) == NULL)
+		{
+			for (i = i - 1; i >= 0; i--)
+				free(*(pgrid + i));
 			return (NULL);
+		}
 		for (j = 0; j < width; j++)
 		{
 			pgrid[i][j] = 0;
