68cc52aa93dfc66f746708a79a4a5f7add2807c9
j-tyler <justin.marsh@holbertonschool.com>
2016-11-09 02:51:02 +0000

    Add solution 5 0x0A

---
+++ 0x0A-malloc_free/5-argstostr.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenate all arguments as a string
+ * @ac: argument count
+ * @av: argument array
+ *
+ * Return: Pointer to string
+ */
+char *argstostr(int ac, char **av)
+{
+	int nulcount, len, i, j;
+	char *p;
+
+	for (i = 0, nulcount = 0, len = 0; nulcount < ac; i++, len++)
+	{
+		if (av[nulcount][i]  == '\0')
+		{
+			nulcount++;
+			i = -1;
+		}
+	}
+
+	p = malloc(len * sizeof(char));
+	if (p == NULL)
+		return (NULL);
+	for (i = 0, j = 0, nulcount = 0; nulcount < ac; i++, j++)
+	{
+		p[j] = av[nulcount][i];
+		if (av[nulcount][i] == '\0')
+		{
+			p[j] = '\n';
+			nulcount++;
+			i = -1;
+		}
+	}
+	return (p);
+}
