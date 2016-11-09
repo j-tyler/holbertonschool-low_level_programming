cb82886c75f2bd8ab14d610adf995f144114a890
j-tyler <justin.marsh@holbertonschool.com>
2016-11-09 18:57:30 +0000

    0 0x0A Return Null on size 0

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -12,6 +12,8 @@ char *create_array(unsigned int size, char c)
 	char *p;
 	unsigned int i;
 
+	if (size == 0)
+		return (NULL);
 	p = malloc(size * sizeof(char));
 	for (i = 0; i < size; i++)
 		*(p + i) = c;
