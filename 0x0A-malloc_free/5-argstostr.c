f11da56cb1d3dcc61d47ea29b35ad08213b9b3ec
j-tyler <justin.marsh@holbertonschool.com>
2016-11-09 20:35:31 +0000

    5 0x0A Null error checking

--- 0x0A-malloc_free/5-argstostr.c
+++ 0x0A-malloc_free/5-argstostr.c
@@ -13,6 +13,9 @@ char *argstostr(int ac, char **av)
 	int nulcount, len, i, j;
 	char *p;
 
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
 	for (i = 0, nulcount = 0, len = 0; nulcount < ac; i++, len++)
 	{
 		if (av[nulcount][i]  == '\0')
