2d1305722a2a1946d43becb0fe66e400b827ad0a
j-tyler <justin.marsh@holbertonschool.com>
2016-11-11 07:20:27 +0000

    Add solution 101 0x0B Multiplication of large numbers

--- 0x0B-more_malloc_free/holberton.h
+++ 0x0B-more_malloc_free/holberton.h
@@ -1,3 +1,4 @@
+int _putchar(char c);
 void *_realloc(void *ptr, unsigned int old_size, unsigned new_size);
 void *_calloc(unsigned int nmemb, unsigned int size);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
