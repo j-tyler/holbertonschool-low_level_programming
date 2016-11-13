bd997093c5bec31656f2444b79083fe8fbb6b726
j-tyler <justin.marsh@holbertonschool.com>
2016-11-13 21:30:41 +0000

    5 0x04 in Go

--- 0x04-pointers_arrays_strings/5-rev_string.go
+++ 0x04-pointers_arrays_strings/5-rev_string.go
@@ -4,7 +4,7 @@ package main
 import "fmt"
 
 func main() {
-	var s string = "Holberton adf̅ School"
+	var s string = "Holberton f̅ bròwn School"
 	fmt.Println(s)
 	rev_string(&s)
 	fmt.Println(s)
@@ -15,6 +15,15 @@ func rev_string(s *string) {
 	r := make([]rune, len(*s))
 	for i, c := range *s {
 		r[len(*s) - 1 - i] = c
+	}
+	*s = string(r)
+}
+
+// Another option, still doesn't preserve all unicode
+func rev_string2(s *string) {
+	r := []rune(*s)
+	for i, j := 0, len(r) - 1; i < len(r)/2; i , j = i + 1, j - 1 {
+		r[i], r[j] = r[j], r[i]
 	}
 	*s = string(r)
 }
