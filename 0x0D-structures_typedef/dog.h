cb8e762f484ddc496be97f9734598fc1913b8330
j-tyler <justin.marsh@holbertonschool.com>
2016-11-14 19:11:59 +0000

    Add solution 3 0x0D

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,5 +1,11 @@
+#ifndef DOG_H
+#define DOG_H
 struct dog {
 	char *name;
 	float age;
 	char *owner;
 };
+void print_dog(struct dog *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+typedef struct dog dog_t;
+#endif
