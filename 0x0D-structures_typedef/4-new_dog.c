e9c51c3411e6388b27b2f0ea02c974a2a072934a
j-tyler <justin.marsh@holbertonschool.com>
2016-11-14 20:45:57 +0000

    Add framework 4 and 5 0x0D

---
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * new_dog - creates a new dog
+ * @name: name of doge
+ * @age: age of doge
+ * @owner: does anyone ever really own a doge?
+ *
+ * Return: Pointer to new doge, NULL if fails
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+}
---
+++ 0x0D-structures_typedef/5-free_dog.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - free memory from a dog structure
+ * @d: pointer to struct dog
+ */
+void free_dog(dog_t *d)
+{
+
+}
