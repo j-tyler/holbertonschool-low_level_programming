c2b9fc96f83270374d1f677710ed1849bc0c128c
j-tyler <justin.marsh@holbertonschool.com>
2016-11-15 03:51:22 +0000

    4 0x0D Fix memory allocation

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -11,34 +11,13 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
-	dog_t dog;
-	int i, len1, len2;
-	char *newname, *newowner;
 
-	new_dog = &dog;
-	len1 = len2 = i = 0;
-	while (name[len1] != '\0')
-		len1++;
-	while (owner[len2] != '\0')
-		len2++;
-
-	newname = malloc(len1 + 1);
-	newowner = malloc(len2 + 1);
-
-	if (newname == NULL)
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
 		return (NULL);
-	if (newowner == NULL)
-		return (NULL);
-	do {
-		newname[i] = name[i];
-	} while (name[i++] != '\0');
-	i = 0;
-	do {
-		newowner[i] = owner[i];
-	} while (owner[i++] != '\0');
 
-	new_dog->name = newname;
+	new_dog->name = name;
 	new_dog->age = age;
-	new_dog->owner = newowner;
+	new_dog->owner = owner;
 	return (new_dog);
 }
