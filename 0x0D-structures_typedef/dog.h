fb5b8119dfa35b30561c8bf3d4f779a793e51a1b
j-tyler <justin.marsh@holbertonschool.com>
2016-11-15 02:30:17 +0000

    0 0x0D Add struct comments

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
-struct dog {
+/**
+ * struct dog - dogs!
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: does anyone ever really own a dog?
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
