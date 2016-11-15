61fdeb43000d196fa0857f707cd635f58075d516
j-tyler <justin.marsh@holbertonschool.com>
2016-11-15 02:42:20 +0000

    2 0x0D Cannot make float NULL

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -17,7 +17,7 @@ void print_dog(struct dog *d)
 			printf("Name: (nil)\n");
 		else
 			printf("Name: %s\n", d->name);
-		if (d->age == NULL)
+		if (d->age < 0)
 			printf("Age: (nil)\n");
 		else
 			printf("Age: %.1f\n", d->age);
