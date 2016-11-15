4bfc6d9c9e4a8cb972af429e6c10423f3c1f16d9
j-tyler <justin.marsh@holbertonschool.com>
2016-11-15 02:40:20 +0000

    2 0x0D New specifications

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -1,12 +1,29 @@
 #include "dog.h"
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * print_dog - prints out the argument of type struct dog
  * @d: pointer to struct dog
  */
 void print_dog(struct dog *d)
 {
-	printf("Name: %s\n", d->name);
-	printf("Age: %.1f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	if (d == NULL)
+	{
+		;
+	}
+	else
+	{
+		if (d->name == NULL)
+			printf("Name: (nil)\n");
+		else
+			printf("Name: %s\n", d->name);
+		if (d->age == NULL)
+			printf("Age: (nil)\n");
+		else
+			printf("Age: %.1f\n", d->age);
+		if (d->owner == NULL)
+			printf("Owner: (nil)\n");
+		else
+			printf("Owner: %s\n", d->owner);
+	}
 }
