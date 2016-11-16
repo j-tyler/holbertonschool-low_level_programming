1885c4cb23bf91d475cebf79111451cf36f5b380
j-tyler <justin.marsh@holbertonschool.com>
2016-11-16 23:30:27 +0000

    2 0x0E Edgecase check

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -16,7 +16,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
-		if (!((*cmp)(array[i]) != 0 && (*cmp)(array[i]) != NULL))
+		if (!((*cmp)(array[i]) == 0 || (*cmp)(array[i]) == NULL))
 			return (i);
 	}
 	return (-1);
