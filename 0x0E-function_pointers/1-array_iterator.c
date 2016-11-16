5bb80901831fa7aa2b42d7f387e0e5d9226f0dab
j-tyler <justin.marsh@holbertonschool.com>
2016-11-16 23:08:49 +0000

    Add solution 1 0x0E

---
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - Execute function on each element of array
+ * @array: int array to iterate over
+ * @size: size of the array
+ * @action: function to use on array
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+		(*action)(array[i]);
+}
