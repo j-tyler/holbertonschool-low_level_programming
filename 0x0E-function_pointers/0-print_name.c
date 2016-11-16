1811c48bb84e1fbe2197e22a0fd042daa23ecced
j-tyler <justin.marsh@holbertonschool.com>
2016-11-16 23:00:56 +0000

    Add solution 0 0x0E

---
+++ 0x0E-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+#include "function_pointers.h"
+/**
+ * print_name - Sends the string to the function specified
+ * @name: String to send to function
+ * @f: function to send string to
+ */
+void print_name(char *name, void(*f)(char *))
+{
+	(*f)(name);
+}
