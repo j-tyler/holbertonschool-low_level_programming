35dbc57ce3facc5927ce1bc91c3bd84ea25cf7b2
j-tyler <justin.marsh@holbertonschool.com>
2016-11-17 06:13:36 +0000

    3 0x0E Check for op length over size 1

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 	if ((*argv[2] == '/' || *argv[2] == '%') && atoi(argv[3]) == 0)
 		printf("Error\n"), exit(100);
 	hold = get_op_func(argv[2]);
-	if (hold == NULL)
+	if (hold == NULL || argv[2][1] != '\0')
 		printf("Error\n"), exit(99);
 	printf("%d\n", ((*hold)(atoi(argv[1]), atoi(argv[3]))));
 	return (0);
