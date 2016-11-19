edc177c6d13cdbe3f2d2b9bdc99ca95fe06c8149
j-tyler <justin.marsh@holbertonschool.com>
2016-11-19 02:34:04 +0000

    1 0x0F - Print separator as a string

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -16,7 +16,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(valist, int));
 		if (i + 1 < n && separator != NULL)
-			printf("%c", *separator);
+			printf("%s", separator);
 	}
 	printf("\n");
 	va_end(valist);
