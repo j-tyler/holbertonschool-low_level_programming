06da14f4f44aaa6248a344eb444e873f776f8c44
j-tyler <justin.marsh@holbertonschool.com>
2016-11-19 02:25:42 +0000

    1 0x0F - Redo NULL checking

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -10,19 +10,15 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list valist;
 	unsigned int i;
-	const char *c;
-
-	if (separator == NULL)
-		c = "";
-	else
-		c = separator;
 
 	va_start(valist, n);
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(valist, int));
-		if (i + 1 < n)
-			printf("%c ", *c);
+		if (i + 1 < n && separator != NULL)
+			printf("%c ", *separator);
+		else if (i + 1 < n)
+			printf(" ");
 	}
 	printf("\n");
 	va_end(valist);
