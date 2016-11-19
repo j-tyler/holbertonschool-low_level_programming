30b6abd2a3703ae65ecc697127c7ffa28c8596b5
j-tyler <justin.marsh@holbertonschool.com>
2016-11-19 02:04:07 +0000

    0 0x0F Fix whitespace

--- 0x0F-variadic_functions/0-sum_them_all.c
+++ 0x0F-variadic_functions/0-sum_them_all.c
@@ -10,12 +10,12 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list args;
-	unsigned int i; 
+	unsigned int i;
 	int sum;
 
 	va_start(args, n);
 	for (i = 0, sum = 0; i < n; i++)
-		sum += va_arg(args, int); 
+		sum += va_arg(args, int);
 	va_end(args);
 
 	return (sum);
