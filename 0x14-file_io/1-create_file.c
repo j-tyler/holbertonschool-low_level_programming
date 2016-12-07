71a18b08536e4f2804579c57b18b77e8c4cd7fad
j-tyler <justin.marsh@holbertonschool.com>
2016-12-07 19:09:56 +0000

    Add solution 1 1x14 create_file

---
+++ 0x14-file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+/**
+ * create_file - create a file
+ * @filename: name of the file
+ * @text_content: content for the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, err, bytes;
+	mode_t mode = S_IRUSR | S_IWUSR;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT, mode);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		err = close(fd);
+		if (err == -1)
+			return (-1);
+		return (1);
+	}
+
+	bytes = 0;
+	while (text_content[bytes])
+		bytes++;
+
+	err = write(fd, text_content, bytes);
+	if (err == -1)
+		return (-1);
+	err = close(fd);
+	if (err == -1)
+		return (-1);
+	return (1);
+}
