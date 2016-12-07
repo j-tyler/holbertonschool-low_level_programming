b0fdb4dccaac29ed06dc3a02e5be9b1815ab2979
j-tyler <justin.marsh@holbertonschool.com>
2016-12-07 21:13:15 +0000

    Add solution 3 0x14 copy from file to file

---
+++ 0x14-file_io/3-cp.c
@@ -0,0 +1,60 @@
+#include "holberton.h"
+/**
+ * _write_to_err - write to std error two strings concatonated
+ * @s1: string 1
+ * @s2: string 2
+ */
+void _write_to_err(char *s1, char *s2)
+{
+	char arr[256];
+	int i, j;
+
+	i = j = 0;
+	while (s1[i])
+		arr[i] = s1[i], i++;
+	while (s2[j])
+		arr[i] = s2[j], i++, j++;
+	arr[i] = '\n';
+
+	write(2, arr, i);
+}
+/**
+ * main - copy the content of one file to another
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: 0
+ */
+int main(int ac, char **av)
+{
+	int fdr, fdw, err, bytes;
+	char buf[1204];
+	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+
+	if (ac != 3)
+		_write_to_err("Usage: cp file_from file_to", ""), exit(97);
+
+	fdr = open(av[1], O_RDONLY);
+	if (fdr == -1)
+		_write_to_err("Error: Can't read from file ", av[1]), exit(98);
+	fdw = open(av[2], O_WRONLY | O_CREAT | O_APPEND, mode);
+	if (fdw == -1)
+		_write_to_err("Error: Can't write to ", av[2]), exit(99);
+
+	err = bytes = 1;
+	while (bytes)
+	{
+		bytes = read(fdr, buf, 1204);
+		if (bytes == -1)
+			_write_to_err("Error: Can't read from file ", av[1]), exit(98);
+		if (bytes > 0)
+		{
+			err = write(fdw, buf, bytes);
+			if (err == -1)
+				_write_to_err("Error: Can't write to ", av[2]), exit(99);
+		}
+	}
+	close(fdr);
+	close(fdw);
+	return (0);
+}
