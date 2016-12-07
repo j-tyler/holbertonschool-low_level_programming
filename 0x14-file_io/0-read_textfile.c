9a79248a1ee369d1db2690ea81fcf990dec0a798
j-tyler <justin.marsh@holbertonschool.com>
2016-12-07 18:54:37 +0000

    Add solution 0 0x14 read_textfile

---
+++ 0x14-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+/**
+ * read_textfile - read a text file and print it to stdout
+ * @filename: filename to read from
+ * @letters: number of letters to print
+ *
+ * Return: number of letters to print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t err, n;
+	char *buf;
+
+	buf = malloc(sizeof(*buf) * letters);
+	if (buf == NULL)
+		return (0);
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	n = read(fd, buf, letters);
+	if (n == -1)
+		return (0);
+
+	err = write(1, buf, n);
+	if (err == -1)
+		return (0);
+	err = close(fd);
+	if (err == -1)
+		return (0);
+	return (n);
+}
---
+++ 0x14-file_io/holberton.h
@@ -0,0 +1,7 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+ssize_t read_textfile(const char *filename, size_t letters);
+#endif
