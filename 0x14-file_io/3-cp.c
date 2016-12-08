34ab6957f72294d587a66f89f80b9a19be79d6cb
j-tyler <justin.marsh@holbertonschool.com>
2016-12-08 03:34:41 +0000

    Revert changes

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -42,7 +42,7 @@ int main(int ac, char **av)
 		dprintf(2, "Error: Can't read from file %s\n", av[1]), exit(98);
 	fdw = open(av[2], O_WRONLY | O_CREAT | O_APPEND, mode);
 	if (fdw == -1)
-		dprintf(2, "Error: Can't write to %s\n", av[2]), close(fdr); exit(99);
+		dprintf(2, "Error: Can't write to %s\n", av[2]), exit(99);
 	err = bytes = 1;
 	while (bytes)
 	{
