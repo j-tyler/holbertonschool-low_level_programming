dfec9f07f9431664410fb4d6b5cb41ff4d0f9090
j-tyler <justin.marsh@holbertonschool.com>
2016-12-08 03:08:33 +0000

    Add a close condition

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -16,10 +16,9 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_CREAT, mode);
 	if (fd == -1)
 		return (-1);
-
 	if (text_content == NULL)
 	{
-		err = close(fd);
+		close(fd);
 		return (1);
 	}
 
@@ -29,7 +28,10 @@ int create_file(const char *filename, char *text_content)
 	if (bytes > 0)
 		err = write(fd, text_content, bytes);
 	if (err == -1)
+	{
+		close(fd);
 		return (-1);
+	}
 	close(fd);
 	return (1);
 }
