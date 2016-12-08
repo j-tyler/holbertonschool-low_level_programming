b833f82c24b9bb77a79686ec71a811b5463b6e24
j-tyler <justin.marsh@holbertonschool.com>
2016-12-08 02:47:29 +0000

    Return -1 if file already exists

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -13,15 +13,13 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT, mode);
+	fd = open(filename, O_WRONLY | O_CREAT | O_EXCL, mode);
 	if (fd == -1)
 		return (-1);
 
 	if (text_content == NULL)
 	{
 		err = close(fd);
-		if (err == -1)
-			return (-1);
 		return (1);
 	}
 
@@ -32,8 +30,6 @@ int create_file(const char *filename, char *text_content)
 		err = write(fd, text_content, bytes);
 	if (err == -1)
 		return (-1);
-	err = close(fd);
-	if (err == -1)
-		return (-1);
+	close(fd);
 	return (1);
 }
