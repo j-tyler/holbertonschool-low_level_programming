a60778f9a2fff3652f9bf3726b9a91a8b5e0e836
j-tyler <justin.marsh@holbertonschool.com>
2016-12-08 06:13:50 +0000

    Add POSIX symbolic constants

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -32,35 +32,35 @@ int main(int ac, char **av)
 	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
 	if (ac != 3)
-		dprintf(2, "Usage: cp file_from file_to\n"), exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	if (av[1] == NULL)
-		dprintf(2, "Error: Can't read from file %s\n", av[1]), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 	if (av[2] == NULL)
-		dprintf(2, "Error: Can't write to %s\n", av[2]), exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	fdr = open(av[1], O_RDONLY);
 	if (fdr == -1)
-		dprintf(2, "Error: Can't read from file %s\n", av[1]), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 	fdw = open(av[2], O_WRONLY | O_CREAT | O_APPEND, mode);
 	if (fdw == -1)
-		dprintf(2, "Error: Can't write to %s\n", av[2]), exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	err = bytes = 1;
 	while (bytes)
 	{
 		bytes = read(fdr, buf, 1204);
 		if (bytes == -1)
-			dprintf(2, "Error: Can't read from file %s\n", av[1]), exit(98);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 		if (bytes > 0)
 		{
 			err = write(fdw, buf, bytes);
 			if (err == -1)
-				dprintf(2, "Error: Can't write to %s\n", av[2]), exit(99);
+				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 		}
 	}
 	err = close(fdr);
 	if (err == -1)
-		dprintf(2, "Error: Can't close fd %d\n", fdr), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdr), exit(100);
 	err = close(fdw);
 	if (err == -1)
-		dprintf(2, "Error: Can't close fd %d\n", fdw), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdw), exit(100);
 	return (0);
 }
