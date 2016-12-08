6c52523da8c3f746bc861f9adf4054a270b6be66
j-tyler <justin.marsh@holbertonschool.com>
2016-12-08 03:16:58 +0000

    Revert Changes

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -17,21 +17,21 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	if (filename == NULL)
-		free(buf); return (0);
+		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		free(buf); return (0);
+		return (0);
 
 	n = read(fd, buf, letters);
 	if (n == -1)
-		free(buf); return (0);
+		return (0);
 	if (n > 0)
 		n = write(1, buf, n);
 	if (n == -1)
-		free(buf); return (0);
+		return (0);
 	err = close(fd);
 	if (err == -1)
-		free(buf); return (0);
+		return (0);
 	free(buf);
 	return (n);
 }
