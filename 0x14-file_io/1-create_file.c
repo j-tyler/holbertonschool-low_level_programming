d4753b5d09858006030557a76402fab36170758a
j-tyler <justin.marsh@holbertonschool.com>
2016-12-08 06:44:42 +0000

    Add 0_TRUNC to file open commands

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -30,7 +30,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT, mode);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, mode);
 	if (fd == -1)
 		return (-1);
 	if (text_content == NULL)
