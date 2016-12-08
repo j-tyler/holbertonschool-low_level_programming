b9b4366c9e57de121946422268d3e3d3e36b528f
j-tyler <justin.marsh@holbertonschool.com>
2016-12-08 02:48:10 +0000

    Revert changes

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT | O_EXCL, mode);
+	fd = open(filename, O_WRONLY | O_CREAT, mode);
 	if (fd == -1)
 		return (-1);
 
