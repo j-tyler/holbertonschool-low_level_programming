e9a3a5736693bb2e2d999ce4b6fbf41e0c2e5b9e
j-tyler <justin.marsh@holbertonschool.com>
2017-01-10 18:49:44 +0000

    0x16 Add: 7-insert_dnodeint.c

---
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,57 @@
+#include "lists.h"
+/**
+ * _create_node_2 - create node at given location
+ * @prev: Previous item on list
+ * @next: item to link this node to
+ * @n: number to store
+ *
+ * Return: Address of node
+ */
+dlistint_t *_create_node_2(dlistint_t *prev, dlistint_t *next, const int n)
+{
+	dlistint_t *node;
+
+	node = malloc(sizeof(dlistint_t));
+	if (node == NULL)
+		return (NULL);
+	node->prev = prev;
+	node->next = next;
+	node->n = n;
+	if (prev != NULL)
+		prev->next = node;
+
+	return (node);
+}
+/**
+ * insert_dnodeint_at_index - Insert node at index given
+ * @h: head of list
+ * @idx: index to insert
+ * @n: value to store in node
+ *
+ * Return: Address of node, NULL on failure
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	unsigned int i;
+	dlistint_t *tmp;
+
+	if (h == NULL)
+		return (NULL);
+	if (n == 0)
+	{
+		*h = _create_node_2((*h)->prev, *h, n);
+		return (*h);
+	}
+	i = 0;
+	tmp = *h;
+	while (1)
+	{
+		if (tmp == NULL)
+			return (NULL);
+		if (i == idx - 1)
+			break;
+		i++;
+		tmp = tmp->next;
+	}
+	return (_create_node_2(tmp, tmp->next, n));
+}
