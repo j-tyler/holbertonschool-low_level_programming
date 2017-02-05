c8646680534a52f9ea6acb5b0220eb5d89db4d03
j-tyler <justin.marsh@holbertonschool.com>
2017-02-05 00:19:55 +0000

    0x19 Add: 4-hash_table_get.c

---
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - Retrieve a value from a key in hash table
+ * @ht: Hashing table
+ * @key: Key to search for
+ * Return: Value attached to key, NULL if key doesn't exist
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL)
+		return (NULL);
+
+	index = hash_djb2((const unsigned char *)key) % ht->size;
+	return ((char *)key_value(ht->array[index], key));
+}
