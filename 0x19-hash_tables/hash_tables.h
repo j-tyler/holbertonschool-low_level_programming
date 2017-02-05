c609a6222bf952a933ae652f70cbfa80668c9116
j-tyler <justin.marsh@holbertonschool.com>
2017-02-05 05:40:36 +0000

    Rename task 6

--- 0x19-hash_tables/hash_tables.h
+++ 0x19-hash_tables/hash_tables.h
@@ -42,5 +42,6 @@ char *key_value(hash_node_t *h, const char *key);
 int key_insert(hash_node_t **h, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
 
 #endif
