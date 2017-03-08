9a0b2a5dfa17e5ea440a663d87cf4bb7aeb6cd0d
j-tyler <justin.marsh@holbertonschool.com>
2017-03-08 04:34:27 +0000

    Update header, add pretty printing

--- 0x1C-binary_trees/binary_trees.h
+++ 0x1C-binary_trees/binary_trees.h
@@ -1,6 +1,11 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -21,6 +26,9 @@ typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
+
+/* Pretty Printing */
+void binary_tree_print(const binary_tree_t *);
 
 /* Task 0 New Node */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
